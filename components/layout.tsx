d5595536fadfb1f50a08d66e0c6aed9062da901d
Quentin <quentin@MacBook-Pro-de-Quentin.local>
2023-07-17 11:00:05 +0200

    add animation

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,15 +1,39 @@
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import Link from "next/link";
 
-export const Layout = ({ children }) =>{
-    return (
-        <motion.main initial={{opacity: 0, y: 150}} animate={{opacity: 1, y: 0}} exit={{opacity: 0, x:-150}}>
-            <nav className="mx-auto max-w-xl">
-                <ul className="flex gap-4">
-                    <li><Link href={"/"}>Home</Link></li>
-                    <li><Link href={"/contact"}>Contact</Link></li>
-                </ul>
-            </nav>
+export const Layout = ({ children }) => {
+  return (
+    <main>
+      <motion.div
+        initial={{ x: 0 }}
+        animate={{ x: "100%" }}
+        exit={{ x: 0 }}
+        transition={{ duration: 0.8 }}
+        className="fixed inset-0 bg-slate-300 z-40"
+      ></motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <nav className="w-full bg-slate-700 text-white"
+            style={{
+                position:"fixed"
+            }}
+        >
+          <ul className="flex gap-4">
+            <li>
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/contact"}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="pt-20">
             {children}
-        </motion.main>
-)}
+        </div>
+      </motion.div>
+    </main>
+  );
+};
